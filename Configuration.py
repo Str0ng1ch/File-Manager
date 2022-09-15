70d4fc9cc03e6bcc30c570879a65e86f9b0adbfd
Str0ng1ch <artemkuzmi@gmail.com>
2022-09-16 01:03:30 +0300

    Fixed Configuration.py

--- Configuration.py
+++ Configuration.py
@@ -3,11 +3,11 @@ import shutil
 
 
 def configuration():
-    if os.path.exists('D:\\Study\\All_python\\everything\\Home'):
+    path = os.path.abspath(os.curdir) + '\\Home\\'
+    if os.path.exists(path):
         shutil.rmtree('Home')
     os.mkdir('Home')
     os.mkdir('Home/User')
     os.mkdir('Home/System')
     os.mkdir('Home/Program Files')
-    path = os.path.abspath(os.curdir) + '\\Home\\'
     return path
